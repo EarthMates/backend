3f6f995405da4444e75d7012e3e38281258cfaf1
EarthMates <163406866+EarthMates@users.noreply.github.com>
2024-06-16 22:00:36 +0200

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 
     'api',
     'rest_framework', 
+    'sendgrid',
     'corsheaders'
 ]
 
@@ -165,3 +166,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = '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'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'jacopo@earthmates.de'
